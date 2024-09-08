a8eabf94b94b8184c52b92c07e8b9a13d827ca3e
pity <2369034170@qq.com>
2024-09-09 02:23:00 +0800

    cxk

--- src/main/java/com/example/musicrecommendationsys/MusicRecommendationSysApplication.java
+++ src/main/java/com/example/musicrecommendationsys/MusicRecommendationSysApplication.java
@@ -3,6 +3,7 @@ package com.example.musicrecommendationsys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test
 @SpringBootApplication
 public class MusicRecommendationSysApplication {
 
